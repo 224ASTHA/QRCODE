cb9b2bcef0b3a2a0709fa36312cb23196c9e76a6
224ASTHA <sanchitabehera2005@gmail.com>
2024-08-16 12:26:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+import QRCode from "react-qr-code";
 import './App.css';
 
 function App() {
+
+  const [url,setUrl] = useState("");
+  const [visible,setVisible] = useState(false);
+  const [fullUrl,setFullUrl] = useState("");
+  const handleGenerator = () => {
+    if(!url){
+      return;
+    }
+    setVisible(true);
+    setFullUrl(url);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>QR Code Generator</h1>
+       <div className = "container">
+        <div className="parent">
+        <div className = "box">
+          <input type="text" placeholder="Enter a URL"
+            value={url}
+            onChange={(e) => {
+              setUrl(e.target.value)
+            }}
+          />
+
+          <button onClick={handleGenerator}>Generate QR Code</button>
+        </div>
+        {visible && (
+          <div className="image">
+            <QRCode value={fullUrl} size={250}/>
+          </div>
+        ) }
+       </div>
+        </div>
     </div>
   );
 }
